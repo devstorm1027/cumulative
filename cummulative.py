7b72fb36ba6d1cc45c189769a854bfc2170a023d
bruno1027 <cb.wglobe@gmail.com>
2018-10-13 13:41:06 +0200

    built chart with Jupyter and ipython

--- cummulative.py
+++ cummulative.py
@@ -2,18 +2,17 @@ import pandas as pd
 
 
 class cumulative(object):
-    def calculate(self):
+    def chart_cummulate(self):
         try:
             df = pd.read_csv('input.csv')
             cols = list(df.columns.values)[1:]
-
             df.loc[:, cols] = df.loc[:, cols].cumsum()
-            df.to_csv('output.csv', index=False)
-
+            df.to_excel('input.csv', index=False)
             print("Successed!")
         except Exception as e:
             print(e)
 
+
 if __name__ == '__main__':
     cum = cumulative()
-    cum.calculate()
+    cum.chart_cummulate()
